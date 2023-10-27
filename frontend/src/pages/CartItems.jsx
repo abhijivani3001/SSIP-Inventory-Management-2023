08655ea5d8dc6f46d6e969922e95ab7c55c874f7
abhijivani3001 <abhijivani3001@gmail.com>
2023-10-27 14:42:46 +0530

    cart items

--- frontend/src/pages/CartItems.jsx
+++ frontend/src/pages/CartItems.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
-import backButton from '../resources/back-button.png';
 import CartItem from '../components/CartItem';
 
 const CartItems = () => {
   const [value, setValue] = useState(12);
   return (
-    <div className='flex flex-col mt-4'>
-      <div className='flex ml-10'>
-        <img src={backButton} alt="arrow" className='mr-9 w-12 h-12 mt-3' />
-        <h1 className='text-5xl font-light tracking-tight text-gray-900'>Cart Items</h1>
-        <div className='mx-8 mt-4'>
-          <div>
+    <div className='mx-8 mt-4'>
+      <div>
         <h1 className='text-6xl font-light'>Cart Items</h1>
       </div>
 
@@ -22,25 +17,7 @@ const CartItems = () => {
         <CartItem value={value} />
       </div>
     </div>
-
-    </div>
-    </div>
-    
   );
 };
 
-
 export default CartItems;
-
-
-
-
-
-
-
-
-
-
-
-
-
