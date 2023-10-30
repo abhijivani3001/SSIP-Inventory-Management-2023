ea2a4651370d94a7ffde4721d49173f62ec10b0d
Bhavy <bhavypdevani31@gmail.com>
2023-10-30 22:35:04 +0530

    order added

--- frontend/src/pages/CartItems.jsx
+++ frontend/src/pages/CartItems.jsx
@@ -2,8 +2,9 @@ import React, { useContext, useEffect, useState } from 'react';
 import CartItem from '../components/Cart/CartItem';
 import Button from '../components/UI/Button';
 import { useCart } from '../store/CartProvider';
-
 import axios from '../api/AxiosUrl';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import the toast library
 
 const CartItems = () => {
   const { cart, dispatch } = useCart();
@@ -12,36 +13,47 @@ const CartItems = () => {
   useEffect(() => {
     setIsCartEmpty(!cart.items.length);
   }, [cart]);
-  // console.log(isCartEmpty,cart.items);
 
   const handleAddToCart = (item) => {
     dispatch({ type: 'ADD_ITEM', payload: item });
   };
+
   const handleRemoveFromCart = (item) => {
     dispatch({ type: 'REMOVE_ITEM', payload: item });
   };
 
   const postElement = async (val) => {
     try {
-      const res = await axios.post('api/order', [{
-        itemId: val._id,
-        quantity: val.amount,
-        delivered: 0,
-        status: 'pending',
-      }]);
+      const res = await axios.post('api/order', [
+        {
+          itemId: val._id,
+          quantity: val.amount,
+          delivered: 0,
+          status: 'pending',
+        },
+      ]);
       console.log(res);
     } catch (error) {
       console.log(error.message);
     }
   };
 
-  const submitHandler =  (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
     console.log(cart);
 
-    cart.items.forEach((val) => {
-      postElement(val);
+    toast.success('Order placed successfully!', {
+      autoClose: 3000,
     });
+
+    if (cart.items.length > 0) {
+      cart.items.forEach((val) => {
+        postElement(val);
+      });
+
+      // Clear the cart
+      dispatch({ type: 'CLEAR_CART' });
+    }
   };
 
   return (
@@ -50,7 +62,6 @@ const CartItems = () => {
         <div className='text-3xl text-center '>Your cart is empty</div>
       )}
 
-      {/* list */}
       {!isCartEmpty && (
         <div>
           <div>
@@ -72,6 +83,7 @@ const CartItems = () => {
           </div>
         </div>
       )}
+      <ToastContainer />
     </div>
   );
 };
