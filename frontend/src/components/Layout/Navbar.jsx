bf324ceb12e863e9a2af9e551b81c3c2d23b00a4
abhijivani3001 <abhijivani3001@gmail.com>
2023-10-30 07:15:33 +0530

    shows cart items on cart icon

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -6,9 +6,17 @@ import notificationImg from '../../resources/notification.png';
 import { useLocation } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 import axios from '../../api/AxiosUrl';
+import { useCart } from '../../store/CartProvider';
 
 const Navbar = () => {
   const location = useLocation();
+  const { cart, dispatch } = useCart();
+  const [amount, setAmount] = useState(cart.items.length || 0);
+  console.log(amount);
+
+  useEffect(() => {
+    setAmount(cart.items.length);
+  }, [cart]);
 
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn;
@@ -35,9 +43,7 @@ const Navbar = () => {
   return (
     <nav className='px-6 py-4 flex justify-between items-center text-gray-900'>
       <div className='text-4xl ml-2 font-semibold'>
-        <Link to='/' >
-          Inventory Management
-        </Link>
+        <Link to='/'>Inventory Management</Link>
       </div>
       <ul className='flex space-x-16 mr-6 align-middle'>
         <div className='flex space-x-6  justify-items-start text-lg'>
@@ -76,49 +82,66 @@ const Navbar = () => {
             </li>
           )}
 
-          {isLoggedIn && <li>
-            <Link
-              to='/order-list'
-              className={`hover:underline ${
-                location.pathname === '/order-list' ? 'navbar-title' : ''
-              }`}
-            >
-              Order List
-            </Link>
-          </li>}
-          {isLoggedIn && <li>
-            <Link
-              to='/placed-order-list'
-              className={`hover:underline ${
-                location.pathname === '/placed-order-list' ? 'navbar-title' : ''
-              }`}
-            >
-              Placed order
-            </Link>
-          </li>}
-          {isLoggedIn && <li>
-            <Link
-              to='/inventory'
-              className={`hover:underline ${
-                location.pathname === '/inventory' ? 'navbar-title' : ''
-              }`}
-            >
-              Inventory
-            </Link>
-          </li>}
+          {isLoggedIn && (
+            <li>
+              <Link
+                to='/order-list'
+                className={`hover:underline ${
+                  location.pathname === '/order-list' ? 'navbar-title' : ''
+                }`}
+              >
+                Order List
+              </Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link
+                to='/placed-order-list'
+                className={`hover:underline ${
+                  location.pathname === '/placed-order-list'
+                    ? 'navbar-title'
+                    : ''
+                }`}
+              >
+                Placed order
+              </Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link
+                to='/inventory'
+                className={`hover:underline ${
+                  location.pathname === '/inventory' ? 'navbar-title' : ''
+                }`}
+              >
+                Inventory
+              </Link>
+            </li>
+          )}
         </div>
 
         <div className='ml-auto flex space-x-4'>
-          {isLoggedIn && <li className='my-auto'>
-            <Link to='/cart'>
-              <img src={cartImg} alt='Cart' className='w-6' />
-            </Link>
-          </li>}
-          {isLoggedIn && <li className='my-auto'>
-            <Link to='/notification'>
-              <img src={notificationImg} alt='Cart' className=' w-6' />
-            </Link>
-          </li>}
+          {isLoggedIn && (
+            <li className='my-auto'>
+              <Link to='/cart'>
+                <div className='flex gap-0'>
+                  <div className='w-6 relative'>
+                    <img src={cartImg} alt='Cart' />
+                  <span className='absolute left-3 bottom-3 bg-teal-700 text-white py-0 px-2 rounded-full z-30'>{amount}</span>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li className='my-auto'>
+              <Link to='/notification'>
+                <img src={notificationImg} alt='Cart' className=' w-6' />
+              </Link>
+            </li>
+          )}
 
           {/* login */}
           {!isLoggedIn && (
@@ -159,7 +182,6 @@ const Navbar = () => {
               </Link>
             </li>
           )}
-          
         </div>
       </ul>
     </nav>
