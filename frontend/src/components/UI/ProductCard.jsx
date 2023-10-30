8cd46f05f851df9cc1f1604c2f1bd0e134373204
Bhavy <bhavypdevani31@gmail.com>
2023-10-30 08:04:39 +0530

    toastify

--- frontend/src/components/UI/ProductCard.jsx
+++ frontend/src/components/UI/ProductCard.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Button from './Button';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProductCard = (props) => {
   const [freqOfItem, setFreqOfItem] = useState(1);
@@ -9,6 +11,17 @@ const ProductCard = (props) => {
   };
   const decrementHandler = () => {
     if (freqOfItem > 1) setFreqOfItem((prev) => prev - 1);
+  };
+
+  const handleAddToCart = () => {
+    // Call the parent component's function to add the item to the cart
+    props.onAddToCart();
+
+    // Show a toast notification
+    toast.success('Item added to cart successfully', {
+      position: 'top-right', // You can customize the position
+      autoClose: 3000, // Close the notification after 3 seconds (adjust as needed)
+    });
   };
 
   return (
@@ -23,34 +36,30 @@ const ProductCard = (props) => {
           {props.title}
         </h5>
 
-        {/* description */}
         <p className='text-gray-500 text-sm my-2'>{props.description}</p>
-        {/* <p className='text-gray-800 text-lg my-2'>Category: {props.category}</p> */}
 
         <div className='flex flex-col gap-1 items-center justify-between my-2 mb-0'>
           <div className='flex justify-evenly gap-2'>
             <label className='text-lg font-semibold'>Amount</label>
-            {/* <Button onClick={decrementHandler}>-</Button> */}
             <input
               type='number'
               className='border-2 border-gray-700 w-12 text-center rounded-lg'
               min={1}
-              // readOnly //
-              // defaultValue={1}
               value={freqOfItem}
               onChange={(e) => {
                 setFreqOfItem(e.target.value);
                 props.amountChangeHandler(e.target.value);
               }}
             />
-            {/* <Button onClick={incrementHandler}>+</Button> */}
           </div>
 
           <div className='mt-2'>
-            <Button onClick={props.onAddToCart}>Add to cart</Button>
+            <Button onClick={handleAddToCart}>Add to cart</Button>
           </div>
         </div>
       </div>
+      {/* Add the ToastContainer from react-toastify */}
+      <ToastContainer />
     </div>
   );
 };
