208c918136d8915d25326c752d3a2c5cfa99b4d4
Bhavy <bhavypdevani31@gmail.com>
2023-11-01 00:01:26 +0530

    orderlist

--- frontend/src/pages/OrderList.jsx
+++ frontend/src/pages/OrderList.jsx
@@ -1,97 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Button from '../components/UI/Button';
-import axios from 'axios';
+import axios from '../api/AxiosUrl';
+import AuthContext from '../store/auth-context';
 
-class OrderList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      selectAll: false,
-    };
-  }
+const OrderList = () => {
+  const [requestedOrders, setRequestedOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isRequestedOrdersAvailable, setIsRequestedOrdersAvailable] =
+    useState(false);
 
-  componentDidMount() {
-    // Fetch all orders with a status of "pending" from the API
-    axios.get('/api/user/users')
+  const authCtx = useContext(AuthContext);
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('api/user')
       .then((response) => {
-        console.log(response);
-        // Filter the orders to get only the ones with status "pending"
-        const pendingOrders = response.data.orders.filter(order => order.status === 'pending');
-        this.setState({ items: pendingOrders });
+        const user = response.data.user;
+        // console.log(user);
+        setUser(user);
       })
       .catch((error) => {
-        console.error('Error fetching orders:', error);
+        console.log(error);
       });
-  }
+  }, []);
 
-  // handleCheckboxChange = (itemId) {
-  //   const updatedItems = this.state.items.map((item) => {
-  //     if (item.itemId === itemId) {
-  //       return { ...item, approved: !item.approved };
-  //     }
-  //     return item;
-  //   });
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.post('api/user/users', {
+          ...user,
+          role: 'employee',
+        });
+        // console.log(res, user);
+        const data = await res.data.users;
+        setRequestedOrders(data);
+        // console.log(data);
+        if (requestedOrders?.length) setIsRequestedOrdersAvailable(true);
+        else setIsRequestedOrdersAvailable(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+      setIsLoading(false);
+    })();
+  }, []);
 
-  //   this.setState({ items: updatedItems });
-  // };
-
-  handleSelectAllChange = () => {
-    const { items, selectAll } = this.state;
-    const updatedItems = items.map((item) => ({
-      ...item,
-      approved: !selectAll,
-    }));
-
-    this.setState({ items: updatedItems, selectAll: !selectAll });
-  };
-
-  render() {
-    const { items, selectAll } = this.state;
-
-    return (
-      <div className='mx-8 mt-4'>
-        <div>
-          <h1 className='text-6xl font-light'>Order List</h1>
+  return (
+    <div className='mx-8 mt-4'>
+      {isLoading && (
+        <div className='text-xl my-auto text-center '>Loading...</div>
+      )}
+      {!isLoading && !isRequestedOrdersAvailable && (
+        <div className='text-3xl text-center'>
+          No more orders are requested!
         </div>
-
-        <div className='my-6'>
-          <label className='mx-4 mt-2 text-xl'>
-            <input
-              className='mx-2'
-              type='checkbox'
-              checked={selectAll}
-              onChange={this.handleSelectAllChange}
-            />
-            Select All
-          </label>
-          <ul>
-            <div className='flex flex-col'>
-              {items.map((item) => (
-                <li key={item.itemId}>
-                  <div className='border-2 m-2 p-2 flex justify-between rounded-xl bg-white'>
-                    <label className='my-auto'>
-                      <input
-                        className='mx-2'
-                        type='checkbox'
-                        checked={item.approved}
-                        onChange={() => this.handleCheckboxChange(item.itemId)}
-                      />
-                      {item.name}
-                    </label>
-                    <div className='flex justify-center align-middle mx-5 space-x-4'>
-                      <div className='my-auto'>{item.quantity}</div>
-                      <Button>Allocate</Button>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </div>
-          </ul>
-        </div>
-      </div>
-    );
-  }
-}
+      )}
+      {!isLoading && isRequestedOrdersAvailable && (
+        <>
+          <div>
+            <h1 className='text-6xl font-light'>Order List</h1>
+          </div>
+          <div className='my-6'>
+            <ul>
+              <div className='flex flex-col'></div>
+            </ul>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default OrderList;
