3f9b24711d51f1b9b8e7d4d7538a2eb7df9359f7
Kamlesh Patel <kamleshpatel12328@gmail.com>
2023-11-02 16:07:48 +0530

    Updated RequestedOrderList file

--- frontend/src/pages/RequestedOrderList.jsx
+++ frontend/src/pages/RequestedOrderList.jsx
@@ -1,27 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import Button from '../components/UI/Button';
-import axios from '../api/AxiosUrl';
-import RequestedOrderData from '../components/RequestedOrderData.jsx/RequestedOrderData';
+import React, { useState, useEffect } from "react";
+import Button from "../components/UI/Button";
+import axios from "../api/AxiosUrl";
+import RequestedOrderData from "../components/RequestedOrderData.jsx/RequestedOrderData";
+import { useCart } from "../store/CartProvider";
+import ROLES from "../constants/ROLES";
 
 const RequestedOrderList = () => {
+  const { cart, dispatch } = useCart();
   const [usersOfRequestedOrders, setUsersOfRequestedOrders] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isRequestedOrdersAvailable, setIsRequestedOrdersAvailable] =
     useState(false);
+  const handleMergeOrder = async () => {
+    const orderMap = new Map();
+    usersOfRequestedOrders.forEach((user) => {
+      user.orders.forEach((order) => {
+        if (orderMap.has(order.itemId)) {
+          const mapItem = orderMap.get(order.itemId);
+          let updatedOrder = {
+            ...mapItem,
+            quantity: order.quantity + mapItem.quantity,
+          };
+          orderMap.set(order.itemId, updatedOrder);
+        } else {
+          orderMap.set(order.itemId, {
+            name: order.name,
+            quantity: order.quantity,
+          });
+        }
+      });
+    });
+    let ordersArray = [];
+    orderMap.forEach((value, key) => {
+      ordersArray.push({
+        _id: key,
+        amount: value.quantity,
+        name: value.name,
+      });
+    });
+    ordersArray.forEach((item) => {
+      dispatch({ type: "ADD_ITEM", payload: item });
+    });
+  };
 
   useEffect(() => {
     (async () => {
       try {
-        const res1 = await axios.get('api/user');
+        const res1 = await axios.get("api/user");
         const user = await res1.data.user;
 
-        const res2 = await axios.post('api/user/users', {
+        let roleOfRequestedUser = "";
+        let roleOfRequestedUser2 = "";
+        if (user.role === ROLES.DEPARTMENT_STORE_MANAGER) {
+          roleOfRequestedUser = ROLES.BRANCH_STORE_MANAGER;
+        } else if (user.role === ROLES.BRANCH_STORE_MANAGER) {
+          roleOfRequestedUser = ROLES.SUB_BRANCH_STORE_MANGER;
+        } else if (user.role === ROLES.SUB_BRANCH_STORE_MANGER) {
+          roleOfRequestedUser = ROLES.EMPLOYEE;
+          roleOfRequestedUser2 = ROLES.SUB_BRANCH_HEAD;
+        }
+        const res2 = await axios.post("api/user/users", {
           ...user,
-          // role: 'employee',
+          role: roleOfRequestedUser,
         });
         // console.log(res2, user);
 
         const data = await res2.data.users;
+
+        // console.log(data);
+        if (roleOfRequestedUser2) {
+          const res3 = await axios.post("api/user/users", {
+            ...user,
+            role: roleOfRequestedUser2,
+          });
+          const tempdata = await res3.data.users;
+          data.push(...tempdata);
+          console.log(data);
+        }
 
         if (data?.length) {
           setIsRequestedOrdersAvailable(true);
@@ -37,26 +92,26 @@ const RequestedOrderList = () => {
   }, []);
 
   return (
-    <div className='mx-8 mt-4'>
+    <div className="mx-8 mt-4">
       {isLoading && (
-        <div className='text-xl my-auto text-center '>Loading...</div>
+        <div className="text-xl my-auto text-center ">Loading...</div>
       )}
       {!isLoading && !isRequestedOrdersAvailable && (
-        <div className='text-3xl text-center'>
+        <div className="text-3xl text-center">
           No more orders are requested!
         </div>
       )}
       {!isLoading && isRequestedOrdersAvailable && (
         <>
-          <div className='flex justify-between'>
-            <h1 className='page-title'>Order List</h1>
-            <h2 className='text-2xl font-light my- gap-2 mr-8'>
-              <div className='flex items-end gap-6 mt-8'>
+          <div className="flex justify-between">
+            <h1 className="page-title">Order List</h1>
+            <h2 className="text-2xl font-light my- gap-2 mr-8">
+              <div className="flex items-end gap-6 mt-8">
                 <div>Requested Quantity</div>
               </div>
             </h2>
           </div>
-          <div className='my-6'>
+          <div className="my-6">
             {usersOfRequestedOrders.map((val) => (
               <RequestedOrderData
                 key={val._id}
@@ -70,8 +125,8 @@ const RequestedOrderList = () => {
               />
             ))}
           </div>
-          <div className=' text-center'>
-            <Button bg='bg-green-400' mb='mb-4'>
+          <div className=" text-center">
+            <Button bg="bg-green-400" mb="mb-4" onClick={handleMergeOrder}>
               Submit
             </Button>
           </div>
