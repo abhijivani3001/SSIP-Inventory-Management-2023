c190250c56fbebb4a5abd0a3e99151dc1a392923
Bhavy <bhavypdevani31@gmail.com>
2023-11-13 23:17:22 +0530

    csv download

--- frontend/src/components/Charts/BarChart.jsx
+++ frontend/src/components/Charts/BarChart.jsx
@@ -1,9 +1,16 @@
 // BarChart.jsx
 import React from 'react';
 import ReactApexChart from 'react-apexcharts';
+import { CSVLink } from 'react-csv';
 
 const BarChart = ({ orderData }) => {
+  console.log(orderData);
   const data = orderData.map(({ x, y }) => ({ x, y }));
+
+  const headers = [
+    { label: 'Date', key: 'x' },
+    { label: 'Quantity', key: 'y' },
+  ];
 
   return (
     <div className='mx-10 my-5'>
@@ -13,13 +20,16 @@ const BarChart = ({ orderData }) => {
             type: 'bar',
           },
           xaxis: {
-            type: 'category',
+            type: 'Date',
           },
         }}
         series={[{ data }]}
         type='bar'
         width={600}
       />
+      <CSVLink data={orderData} headers={headers} className='border-gray-500 text-black p-2 border hover:bg-blue-800 hover:text-white rounded-2xl bg-blue-400 hover:border-blue-700'>
+        Download CSV
+      </CSVLink>
     </div>
   );
 };
