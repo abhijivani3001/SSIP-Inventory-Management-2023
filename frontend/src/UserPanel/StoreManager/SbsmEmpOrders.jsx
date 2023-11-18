420d768745bf406886160e501dc119389d88e5b7
Bhavy <bhavypdevani31@gmail.com>
2023-11-18 17:28:53 +0530

    sbsm below user order done

--- frontend/src/UserPanel/StoreManager/SbsmEmpOrders.jsx
+++ frontend/src/UserPanel/StoreManager/SbsmEmpOrders.jsx
@@ -1,15 +1,14 @@
-// SbsmEmpOrder.jsx
 import React, { useState, useEffect } from 'react';
 import axios from '../../api/AxiosUrl';
 import UserDataCard from '../Admin/UserDataCard';
 import { FaSearch } from 'react-icons/fa';
 import { findBelowUsers } from '../../components/Helper/Helper';
 
-
 const SbsmEmpOrder = () => {
   const [userData, setUserData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedRole, setSelectedRole] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -37,28 +36,55 @@ const SbsmEmpOrder = () => {
     (user.phone || '').toString().includes(searchTerm)
   );
 
+  const roleWiseUsers = {};
+  filteredUsers.forEach((user) => {
+    const { role } = user;
+    if (!roleWiseUsers[role]) {
+      roleWiseUsers[role] = [];
+    }
+    roleWiseUsers[role].push(user);
+  });
+
+  const handleRoleChange = (event) => {
+    setSelectedRole(event.target.value);
+  };
+
   return (
     <div className='mx-8 mt-4'>
       <div className='flex items-center justify-between mb-4'>
         <h1 className='page-title'>All registered users</h1>
+        <div className='flex items-center'>
+          <label htmlFor="roleSelect" className='mr-2 text-lg'>Select Role:</label>
+          <select
+            id="roleSelect"
+            value={selectedRole}
+            onChange={handleRoleChange}
+            className='p-2 border rounded-lg'
+          >
+            <option value="">All</option>
+            {Object.keys(roleWiseUsers).map((role) => (
+              <option key={role} value={role}>
+                {role}
+              </option>
+            ))}
+          </select>
+        </div>
         <div className='flex items-center border rounded-lg px-3'>
-          <FaSearch />
+          <FaSearch className='text-xl text-gray-700' />
           <input
             type='text'
             placeholder='Search user'
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className='bg-transparent outline-none ml-2'
+            className='bg-transparent border-none outline-none mx-2'
           />
         </div>
       </div>
       {isLoading ? (
         <div className='text-xl my-auto mt-8 text-center '>Loading...</div>
       ) : (
-        <div className='text-3xl border border-gray-400 py-5 px-10 rounded-lg shadow-xl my-10'>
-          {filteredUsers.map((user) => (
-            <UserDataCard user={user} key={user.id} />
-          ))}
+        <div className='text-3xl border border-gray-400 py5 px-10 rounded-lg shadow-xl my-10'>
+          <UserDataCard users={roleWiseUsers[selectedRole] || filteredUsers} />
         </div>
       )}
     </div>
---
+++ frontend/src/UserPanel/StoreManager/UserDataCardSbsm.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+const formatDate = (dateString) => {
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  const formattedDate = new Date(dateString).toLocaleDateString('en-GB', options);
+  return formattedDate;
+};
+
+const UserDataCardSbsm = (props) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleViewDetails = () => {
+    setShowDetails((prevShowDetails) => !prevShowDetails);
+  };
+
+  return (
+    <div>
+      {props.users.map((user, index) => (
+        <div key={index}>
+          <div className='p-2 flex justify-between items-center'>
+            <div>
+              <div className='mx-0 p-1'>
+                {user.name} <span className='text-gray-500 text-xl'>({user.role})</span>{' '}
+              </div>
+              <div className='text-xl ml-7'>
+                Branch: <span className='text-gray-500 text-xl '>{user.branch}</span>
+              </div>
+              <div className='text-xl ml-7'>
+                Email: <span className='text-gray-500 mr-14 text-xl'>{user.email}</span>
+                Phone: <span className='text-gray-500 text-xl'>{user.phone}</span>
+              </div>
+            </div>
+            <div>
+              <button
+                onClick={handleViewDetails}
+                className='bg-blue-500 text-gray-900 text-xl px-1 py-1 rounded-xl m-3 p-1'
+              >
+                {showDetails ? 'View Less' : 'View Details'}
+              </button>
+            </div>
+          </div>
+          {showDetails && (
+            <div className="mt-3">
+              {user.bulkOrders.length > 0 ? (
+                <table className="w-full border-collapse border border-gray-400 mt-2">
+                  <thead>
+                    <tr className="bg-gray-200">
+                      <th className="border border-gray-400 p-2 text-xl">Order Name</th>
+                      <th className="border border-gray-400 p-2 text-xl">Quantity</th>
+                      <th className="border border-gray-400 p-2 text-xl">Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {user.bulkOrders.flatMap((bulkOrder) =>
+                      bulkOrder.orders.map((order) => (
+                        <tr key={order.orderId}>
+                          <td className="border border-gray-400 p-2 text-xl">{order.name}</td>
+                          <td className="border border-gray-400 p-2 text-xl">{order.quantity}</td>
+                          <td className="border border-gray-400 p-2 text-xl">
+                            {formatDate(bulkOrder.updatedAt)}
+                          </td>
+                        </tr>
+                      ))
+                    )}
+                  </tbody>
+                </table>
+              ) : (
+                <p className="text-xl text-black p-2 mx-20">No orders placed by {user.name}.</p>
+              )}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UserDataCardSbsm;
