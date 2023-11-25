ae637a24cb96086e49b75e7b8e61fc0fde8ad4a5
Kamlesh Patel <kamleshpatel12328@gmail.com>
2023-11-25 11:37:28 +0530

    budget ui updated

--- frontend/src/pages/BudgetPlanning/PlacedPlanning/AddPlannedProductThree.jsx
+++ frontend/src/pages/BudgetPlanning/PlacedPlanning/AddPlannedProductThree.jsx
@@ -18,7 +18,7 @@ const AddPlannedProductThree = (props) => {
       ]);
       console.log(res);
 
-      toast.success('Item added to inventory successfully', {
+      toast.success('Item added to Plan successfully', {
         position: 'top-right',
         autoClose: 1500,
         style: {
@@ -33,27 +33,27 @@ const AddPlannedProductThree = (props) => {
   };
 
   return (
-    <div className='w-72 bg-white border border-gray-200 rounded-lg shadow-lg mx-2 my-4'>
+    <div className="w-72 bg-white border border-gray-200 rounded-lg shadow-lg mx-2 my-4">
       <img
-        className='p-8 rounded-t-lg h-48 m-auto'
+        className="p-8 rounded-t-lg h-48 m-auto"
         src={props.imageUrl}
-        alt='product image'
+        alt="product image"
       />
-      <div className='px-5 pb-5 mt-4'>
-        <h5 className='text-xl font-semibold tracking-tight text-gray-900'>
+      <div className="px-5 pb-5 mt-4">
+        <h5 className="text-xl font-semibold tracking-tight text-gray-900">
           {props.title}
         </h5>
 
-        <p className='text-gray-500 text-sm my-2'>{props.description}</p>
+        <p className="text-gray-500 text-sm my-2">{props.description}</p>
 
-        <div className='flex flex-col gap-1 items-center justify-between my-2 mb-0'>
-          <div className='flex justify-evenly gap-2'>
-            <label className='text-lg font-semibold'>Amount</label>
+        <div className="flex flex-col gap-1 items-center justify-between my-2 mb-0">
+          <div className="flex justify-evenly gap-2">
+            <label className="text-lg font-semibold">Amount</label>
             <input
               id={props.id}
-              name='amount'
-              type='number'
-              className='border-2 border-gray-700 w-12 h-7 p-0 my-auto text-center rounded-lg'
+              name="amount"
+              type="number"
+              className="border-2 border-gray-700 w-12 h-7 p-0 my-auto text-center rounded-lg"
               min={1}
               value={freqOfItem}
               onChange={(e) => {
@@ -62,8 +62,8 @@ const AddPlannedProductThree = (props) => {
             />
           </div>
 
-          <div className='mt-2'>
-            <button className='blue_btn' onClick={addToInventoryHandler}>
+          <div className="mt-2">
+            <button className="blue_btn" onClick={addToInventoryHandler}>
               Add to Your Plan
             </button>
           </div>
